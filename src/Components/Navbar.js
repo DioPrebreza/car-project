30a48e556e8e59e139eb45788628df121b558032
Dionis Prebreza <52274063+DioPrebreza@users.noreply.github.com>
2023-02-06 12:53:05 +0100

    changes to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,25 +5,12 @@ import classes from "./Header.module.css";
 const Navbar = () => {
   return (
     <div className={classes.nav}>
-      <img src={require("../Images/car-logo.png")} alt="logo" />
-      <ul className={classes.navList}>
-        {/* <li className={classes.navLinks}>
-          <a href="/" className={classes.aStyle}>
-            Ballina
-          </a>
-        </li>
-        <li className={classes.navLinks}>
-          <a href="/" className={classes.aStyle}>
-            Zgjedh Modelin
-          </a>
-        </li> */}
-        <li className={classes.login}>
-          <input type="email" placeholder="Email:"></input>
-          <input type="text" placeholder="Password:"></input>
-          <button>Login</button>
-          <button style={{ display: "none" }}>Sign Up</button>
-        </li>
-      </ul>
+      <img src={require("../Images/car-logo-vertical.png")} alt="logo" />
+      <div className={classes.navList}>
+        <div className={classes.login}>
+          <button>Abonohu Tani!</button>
+        </div>
+      </div>
     </div>
   );
 };
