d1887f506975ffd5e1fe231f8adde8b9e7d1103b
Dionis Prebreza <52274063+DioPrebreza@users.noreply.github.com>
2023-02-07 15:46:25 +0100

    starting listing, not working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
 
 import classes from "./Header.module.css";
+import LandingList from "./List/LandingList";
 import Navbar from "./Navbar";
 import Slider from "./Slider";
 
@@ -9,13 +9,12 @@ const Header = () => {
   return (
     <div className={classes.header}>
       <Navbar />
-      {/* <Container fluid="md"> */}
       <Slider />
       {/* </Container> */}
       {/* <button className={classes.headerButton}>
         <span class={classes.btnText}>Vazhdoni më poshtë</span>
       </button> */}
-      <div style={{ height: "1000px" }}>yo</div>
+      <LandingList />
     </div>
   );
 };
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
+// import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABPQK3CV3Lrlv3yg62fnlWUmePvxvJju4",
-  authDomain: "mekaniku-21054.firebaseapp.com",
-  projectId: "mekaniku-21054",
-  storageBucket: "mekaniku-21054.appspot.com",
-  messagingSenderId: "403881422556",
-  appId: "1:403881422556:web:547283ddd0f787efa78e19",
-  measurementId: "G-BK8L3L13XZ",
+  apiKey: "AIzaSyAG4-Vk01jMFk09LHHoqz3xC2BZbXK56cI",
+  authDomain: "mekaniku-55a7d.firebaseapp.com",
+  databaseURL:
+    "https://mekaniku-55a7d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mekaniku-55a7d",
+  storageBucket: "mekaniku-55a7d.appspot.com",
+  messagingSenderId: "443521690395",
+  appId: "1:443521690395:web:34fa817684de22eaefc1be",
+  measurementId: "G-RT615NN2QX",
 };
 
 // init firebase
@@ -18,6 +22,7 @@ firebase.initializeApp(firebaseConfig);
 // init services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+// const projectStorage = firebase.storage();
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp;
